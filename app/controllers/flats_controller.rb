16979169712e50315d29118d3fe9e046c7f8b82b
Samy Rjob <s.rjob@audencia.com>
2022-12-14 15:13:16 +0100

    yeah modif home

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,11 @@
 class FlatsController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[index show]
+
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+  end 
+
 end
